8358d1415982af58fafec60445ff4747052dc81e
Jorge Gonzales <gonzales823@hotmail.com>
2026-07-28 19:57:40 -0500

    cabios para main

--- src/Services/Identity/DogPlatform.Identity.API/Program.cs
+++ src/Services/Identity/DogPlatform.Identity.API/Program.cs
@@ -4,11 +4,31 @@ using DogPlatform.Identity.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "DogPlatform Identity API",
+        Version = "v1"
+    });
+});
+
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddSingleton(TimeProvider.System);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "DogPlatform Identity API v1");
+        options.RoutePrefix = "swagger";
+    });
+}
 
 app.UseHttpsRedirection();
 
